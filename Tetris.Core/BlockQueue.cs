e335cbfffdd5f0287df5f2e175134a0bcbc88d01
Jaden <jadenwrightmaurais@gmail.com>
2025-12-23 12:18:26 -0500

    Refectored project anf getting ready to add unit tests

--- Tetris.Core/BlockQueue.cs
+++ Tetris.Core/BlockQueue.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Windows.Navigation;
 
-namespace Tetris
+namespace Tetris.Core
 {
     public class BlockQueue
     {
---
+++ Tetris.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tetris.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
