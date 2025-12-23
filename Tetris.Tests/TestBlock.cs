2570b1652c1c4e98e1f2fd3ec03b9fed89ca39bb
Jaden <jadenwrightmaurais@gmail.com>
2025-12-23 18:21:01 -0500

    Block Tests

---
+++ Tetris.Tests/TestBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tetris.Core;
+
+namespace Tetris.Tests
+{
+    public class TestBlock: Block
+    {
+        protected override Position[][] Tiles => new []
+        {
+            new [] { new Position(0,0), new Position(0,1)},
+            new [] { new Position(0,0), new Position(0,1) },
+
+        };
+        public override int Id => 99;
+        protected override Position StartOffset => new Position(5, 3);
+
+    }
+}
