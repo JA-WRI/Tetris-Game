8cd8dc7ed94b7038094fad7f1a64a81ff9aa1945
Jaden <jadenwrightmaurais@gmail.com>
2025-12-23 19:14:03 -0500

    More Block Tests

--- Tetris.Tests/BlockTests.cs
+++ Tetris.Tests/BlockTests.cs
@@ -9,8 +9,36 @@ namespace Tetris.Tests
         {
             var block = new TestBlock();
             var positions = block.TilePostions().ToList();
-            Assert.Contains(positions, p=> p.Rows == 5 && p.Columns ==3 );
+            Assert.Contains(positions, p => p.Rows == 5 && p.Columns == 3);
 
+        }
+
+        [Fact]
+        public void Move_UpdatesTilePositions()
+        {
+            var block = new TestBlock();
+            block.Move(2, 3);
+            var positions = block.TilePostions().ToList();
+            Assert.Contains(positions, p => p.Rows == 7 && p.Columns == 6);
+        }
+        [Fact]
+        public void RotateCW_UpdatesTilePositions()
+        {
+            var block = new TestBlock();
+            block.RotateCW();
+            var positions = block.TilePostions().ToList();
+            Assert.Contains(positions, p => p.Rows == 5 && p.Columns == 3);
+            Assert.Contains(positions, p => p.Rows == 5 && p.Columns == 4);
+        }
+
+        [Fact]
+        public void RotateCCW_UpdatesTilePositions()
+        {
+            var block = new TestBlock();
+            block.RotateCCW();
+            var positions = block.TilePostions().ToList();
+            Assert.Contains(positions, p => p.Rows == 5 && p.Columns == 3);
+            Assert.Contains(positions, p => p.Rows == 5 && p.Columns == 4);
         }
     }
 }
